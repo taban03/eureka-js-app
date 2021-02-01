f706eb9a25149503832cb1c49d6ab8f33db03608
at670475 <andrea.tabone@broadcom.com>
2021-02-01 17:18:27 +0100

    Externalize service configuration

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const fs = require('fs')
     , keyFile = path.resolve(__dirname, '../ssl/localhost.keystore.key')
     , caFile = path.resolve(__dirname, '../ssl/localhost.pem')
 
-const server = require('./server');
-server.startServer();
 
 
 let tlsOptions = {
@@ -16,48 +14,9 @@ let tlsOptions = {
     ca: fs.readFileSync(caFile)
 }
 
-// example configuration
 const client = new Eureka({
-    // application instance information
-    instance: {
-        instanceId: "localhost:samplenodeservice:8081",
-        app: "SAMPLENODESERVICE",
-        hostName: "localhost",
-        ipAddr: "127.0.0.1",
-        vipAddress: "samplenodeservice",
-        secureVipAddress: "samplenodeservice",
-        status: "UP",
-        port: {
-            "$": 0,
-            "@enabled": false
-        },
-        securePort: {
-            "$": 8081,
-            "@enabled": true
-        },
-        dataCenterInfo: {
-            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-            name: 'MyOwn'
-        },
-        metadata: {
-            "routed-services.1.gateway-url": "/api/v1",
-            "routed-services.1.service-url": "/",
-
-            'mfaas.discovery.catalogUiTile.id': 'samplenodeservice',
-            'mfaas.discovery.catalogUiTile.title': 'Zowe Sample Node Service',
-            'mfaas.discovery.catalogUiTile.description': 'Sample service running '
-                + 'under NodeJS and registering into API Mediation Layer\'s Discovery service ',
-            'mfaas.discovery.catalogUiTile.version': '1.0.0',
-
-            'mfaas.discovery.service.title': 'Zowe Sample Node Service',
-            'mfaas.discovery.service.description': 'The Proxy Server is an HTTP, '
-                + 'HTTPS, and Websocket server built upon NodeJS and ExpressJS.',
-
-            'mfaas.api-info.apiVersionProperties.v1.title': 'Zowe Sample Node Service API',
-            'mfaas.api-info.apiVersionProperties.v1.description': 'An API for the Zowe Sample Node Service ',
-            'mfaas.api-info.apiVersionProperties.v1.version': '1.0.0'
-        },
-    },
+    filename: 'eureka-client',
+    cwd: '/Users/at670475/IntelliJProjects/nodejs_enabler/eureka-js-app/helloworld-expressjs',
     eureka: {
         // eureka server host / port
         host: 'localhost',
@@ -70,4 +29,15 @@ const client = new Eureka({
     }
 });
 
-client.start();
+function connectToEureka() {
+    client.logger.level('debug');
+    client.start(function(error) {
+        console.log(JSON.stringify(error) || 'Eureka registration complete');   });
+}
+
+connectToEureka();
+
+module.exports = {connectToEureka, tlsOptions};
+
+
+
